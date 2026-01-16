8670243a1943757e823380ec216ffb00646028ff
Rui-s0s <segovia.rodrigo2002@gmail.com>
2026-01-16 13:42:14 -0300

    Algo

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,18 @@
 
 use axum::response::IntoResponse;
 use axum::extract::{Query, Path};
-use axum::{routing::get, Router, response::Html};
+use axum::{Router, response::Html};
+use axum::routing::{get, get_service};
 use std::net::SocketAddr;
 use serde::Deserialize;
+use tower_http::services::ServeDir;
+
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-    .route("/hello2/{name}", get(handler_hello2))
-    .route("/hello", get(handler_hello));
+    .merge(routes_hello())
+    .fallback_service(routes_static());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> LISTENING on {addr}\n");
@@ -20,11 +23,20 @@ async fn main() {
         .unwrap();
 }
 
+fn routes_static() -> Router {
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
+}
+
 #[derive(Debug, Deserialize)]
 struct HelloParams {
     name: Option<String>,
 }
 
+fn routes_hello() -> Router {
+    Router::new()
+    .route("/hello2/{name}", get(handler_hello2))
+    .route("/hello", get(handler_hello))
+}
 // `/hello?name=Estefi`
 async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
