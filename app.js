65e5c7bc8c021fc68867f35d2ab78e9a6d14fef2
Rui-s0s <segovia.rodrigo2002@gmail.com>
2026-01-20 13:06:44 -0300

    Controllers y Models creados

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
 app.use('/', routes)
+app.listen(3000, () => console.log('Server running on http://localhost:3000'))
